dcf36ea40225c157b3230e76e61709b0db4fe4ea
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-04-22 20:25:04 +0800

    [dev/main] add util for path manipulation

---
+++ src/PathUtil.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Lumi\LumiPHP;
+
+class PathUtil {
+    public static function getParamNames(string $rawPath) {
+        preg_match_all('/\{([a-zA-Z0-9]+)\}/', $rawPath, $paramNames);
+        return $paramNames[1];
+    }
+
+    public static function toRegexPath(string $rawPath) {
+        return preg_replace('/\{([a-zA-Z0-9]+)\}/', '([a-zA-Z0-9]+)', $rawPath);
+    }
+
+    public static function isMatch(string $rawPath, string $uri) {
+        $regexPath = self::toRegexPath($rawPath);
+        if(!preg_match("#^$regexPath$#", $uri, $matches)){
+            return null;
+        }
+        array_shift($matches);
+        return $matches;
+    }
+}
