1119dd8fcee967fb3894240cd5442c86232e4be9
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-04-22 20:28:24 +0800

    [dev/main] update test app

--- test/application-test.php
+++ test/application-test.php
@@ -11,4 +11,8 @@ $app->get('/', function (Context $ctx) {
     $ctx->res->text('Hello World');
 });
 
+$app->get('/users/{id}', function (Context $ctx) {
+    $ctx->res->text('Hello ' . $ctx->req->getParam('id'));
+});
+
 $app->run();
