8a705d4d2491e43215cfdca371d0534ea393c317
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-04-22 20:26:46 +0800

    [dev/main] update request object: add method for get path param

--- src/Request.php
+++ src/Request.php
@@ -3,5 +3,18 @@
 namespace Lumi\LumiPHP;
 
 class Request {
+    private string $path;
+    private array $matches;
 
+    public function __construct(string $path, array $matches) {
+        $this->path = $path;
+        $this->matches = $matches;
+    }
+
+    public function getParam(string $key): string {
+        $paramNames = PathUtil::getParamNames($this->path);
+        $params = array_combine($paramNames, $this->matches);
+
+        return $params[$key];
+    }
 }
