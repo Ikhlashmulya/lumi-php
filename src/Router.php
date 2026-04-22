31503b0c494decf65e48053904fc26bf58802eee
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-04-22 20:24:08 +0800

    [dev/main] add router object to handle routing

---
+++ src/Router.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Lumi\LumiPHP;
+
+class Router {
+    private array $routes;
+
+    public function __construct() {
+        $this->routes = array();
+    }
+
+    public function add(string $method, string $path, callable $handler) {
+        $this->routes[$method][$path] = $handler;
+    }
+
+    public function has(string $method, string $uri) {
+        foreach ($this->routes[$method] as $path => $handler) {
+            $matches = PathUtil::isMatch($path, $uri);
+            if ($matches !== null) {
+                return [$path, $matches, $handler];
+            }
+        }
+        return [null, null, null];
+    }
+
+}
