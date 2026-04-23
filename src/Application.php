ed9a406a73e244800f746ae5ca50aa32668cd1fa
Ikhlashmulya <nurahmanmulya@gmail.com>
2026-04-23 19:15:59 +0800

    [dev/main] add more http method

--- src/Application.php
+++ src/Application.php
@@ -13,6 +13,34 @@ class Application {
         $this->router->add('GET', $path, $handler);
     }
 
+    public function post(string $path, callable $handler) {
+        $this->router->add('POST', $path, $handler);
+    }
+
+    public function put(string $path, callable $handler) {
+        $this->router->add('PUT', $path, $handler);
+    }
+
+    public function patch(string $path, callable $handler) {
+        $this->router->add('PATCH', $path, $handler);
+    }
+
+    public function delete(string $path, callable $handler) {
+        $this->router->add('DELETE', $path, $handler);
+    }
+
+    public function trace(string $path, callable $handler) {
+        $this->router->add('TRACE', $path, $handler);
+    }
+
+    public function options(string $path, callable $handler) {
+        $this->router->add('OPTIONS', $path, $handler);
+    }
+
+    public function head(string $path, callable $handler) {
+        $this->router->add('HEAD', $path, $handler);
+    }
+
     public function run() {
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $method = $_SERVER['REQUEST_METHOD'];
